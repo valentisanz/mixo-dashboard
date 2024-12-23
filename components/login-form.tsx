1b72225ad74ac4932734900fee93ac5e199e3184
valentisanz <valentisanz99@gmail.com>
2024-12-23 02:36:01 +0100

    Update main page

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -5,23 +5,47 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Image from "next/image";
+import { useAuth } from "@/hooks/useAuth";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { signIn, useSession } from "next-auth/react";
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"div">) {
+  const { data: session, status } = useSession();
   const router = useRouter();
+  const [error, setError] = useState<string>();
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const email = formData.get("email") as string;
+  useEffect(() => {
+    if (session) {
+      router.push("/dashboard");
+    }
+  }, [session, router]);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const username = formData.get("username") as string;
     const password = formData.get("password") as string;
-    router.push("/dashboard");
+
+    const res = await signIn("credentials", {
+      username,
+      password,
+      redirect: false,
+    });
+    if (res?.error) {
+      setError("Usuario o contraseña inválidos");
+    } else {
+      router.push("/dashboard/machines");
+    }
   };
 
-  return (
+  return status === "loading" ? (
+    <h1>Loading...</h1>
+  ) : (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader className="flex items-center">
@@ -34,22 +58,18 @@ export function LoginForm({
         </CardHeader>
         <CardContent>
           <form onSubmit={handleSubmit}>
-            <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-3">
               <div className="grid gap-2">
-                <Label htmlFor="email">Usuario</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  name="email"
-                  placeholder="m@example.com"
-                />
+                <Label htmlFor="username">Usuario</Label>
+                <Input id="username" type="text" name="username" />
               </div>
               <div className="grid gap-2">
                 <Label htmlFor="password">Contraseña</Label>
                 <Input id="password" type="password" name="password" />
               </div>
+              {error && <p className="text-red-500 text-sm m-0">{error}</p>}
               <Button type="submit" className="w-full">
-                Acceder
+                {status ? "Iniciando sesión..." : "Acceder"}
               </Button>
             </div>
           </form>
