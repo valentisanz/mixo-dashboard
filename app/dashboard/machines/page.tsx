c514bad97f622a3812f3859ee0fc37ed206cf34a
valentisanz <valentisanz99@gmail.com>
2024-12-23 11:26:25 +0100

    Update page.tsx

--- app/dashboard/machines/page.tsx
+++ app/dashboard/machines/page.tsx
@@ -1,7 +1,10 @@
 "use client";
+import Message from "@/components/custom-skeleton";
 import { MachineCard } from "@/components/machine-card";
+import { Skeleton } from "@/components/ui/skeleton";
 import useFetch from "@/hooks/useFetch";
 import { MachineResponse } from "@/models/Machine";
+import { useMemo } from "react";
 
 export default function Page() {
   const {
@@ -10,24 +13,24 @@ export default function Page() {
     executeFetch,
   } = useFetch<MachineResponse[]>("/api/machines", "GET");
 
+  const content = useMemo(() => {
+    if (loading) return <Skeleton className="h-[200px] w-[350px] rounded-xl" />;
+
+    if (!machines || machines.length === 0) {
+      return <Message message="No hay máquinas disponibles" />;
+    }
+
+    return machines.map((machine) => (
+      <MachineCard key={machine._id} {...machine} updateData={executeFetch} />
+    ));
+  }, [loading, machines, executeFetch]);
+
   return (
     <div>
       <h1 className="text-2xl sm:text-3xl font-bold mb-4 sm:mb-6">Máquinas</h1>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : !machines || machines.length === 0 ? (
-        <h1>No hay máquinas disponibles</h1>
-      ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3 gap-4 sm:gap-6">
-          {machines.map((machine) => (
-            <MachineCard
-              key={machine._id}
-              {...machine}
-              updateData={executeFetch}
-            />
-          ))}
-        </div>
-      )}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3 gap-4 sm:gap-6">
+        {content}
+      </div>
     </div>
   );
 }
